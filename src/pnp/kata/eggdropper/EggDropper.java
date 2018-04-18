12c951f0cb8b776bc00a0ddc3e77a5ac963f0c29
Unknown <sergio.montanes.vilar@gmail.com>
2018-04-19 00:04:00 +0200

    Implementando el método minEggDropper100
    Se implementa el método que hace que los tests pasen correctamente.

--- src/pnp/kata/eggdropper/EggDropper.java
+++ src/pnp/kata/eggdropper/EggDropper.java
@@ -1,5 +1,7 @@
 package pnp.kata.eggdropper;
 
+import pnp.kata.eggdropper.exceptions.CriticalFloorIsToLowException;
+
 public class EggDropper {
 
 	/**
@@ -7,10 +9,56 @@ public class EggDropper {
 	 * 
 	 * En esta versión, la manera más eficiente de encontrar la
 	 * respuesta es realizar una búsqueda binaria, con coste O(log(n)).
+	 * 
+	 * Puesto que disponemos de 100 huevos, el algoritmo nunca los romperá
+	 * todos, por lo que es innecesario llevar la cuenta de los
+	 * que todavía siguen "vivos".
 	 */
-	public int minEggDropper100(int criticalFloor) {
+	public int minEggDropper100(int criticalFloor) throws CriticalFloorIsToLowException {
+		if(criticalFloor < 1) {
+			throw new CriticalFloorIsToLowException();
+		}
 		
-		return 0;
+		// int survivingEggs = 100; // Innecesario
+		
+		int minFloor = 1;
+		int maxFloor = 100;
+		int minDrops100 = 0;
+		int nextDropFloor;
+		
+		/*
+		 * La variable 'searchingCriticalFloor' terminará conteniendo el valor del criticalFloor,
+		 * correctamente calculado.
+		 * 
+		 * Aunque el enunciado hace un recordatorio de que el desafio no es encontrar este número,
+		 * creo que al menos es interesante tener una variable que termine conteniéndolo
+		 * (aun a pesar de que la variable no tenga uso en el método).
+		 */
+		@SuppressWarnings("unused")
+		int searchingCriticalFloor = calculateAverage(minFloor, maxFloor);
+		
+		while(maxFloor >= minFloor) {
+			nextDropFloor = calculateAverage(minFloor, maxFloor);
+			minDrops100++;
+			if(dropAnEggAndItBreaks(nextDropFloor, criticalFloor)) {
+				// survivingEggs--; // Innecesario
+				maxFloor = nextDropFloor - 1;
+			} else {
+				searchingCriticalFloor = nextDropFloor;
+				minFloor = nextDropFloor + 1;
+			}
+		}
+		
+		return minDrops100;
 	}
 
+	private int calculateAverage(int minFloor, int maxFloor) {
+		return (minFloor + maxFloor) / 2;
+	}
+
+	private boolean dropAnEggAndItBreaks(int dropFloor, int criticalFloor) {
+		if(dropFloor > criticalFloor)
+			return true;
+		return false;
+	}
 }
